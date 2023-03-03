aae7fe5abdcaa8aff5d6cb3234e0db27b27d27a7
Jung Yunho <a01049048063@gmail.com>
2023-03-03 17:52:35 +0900

    chore: Springboot inital setting

---
+++ src/main/java/com/example/techere/TechereApplication.java
@@ -0,0 +1,13 @@
+package com.example.techere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechereApplication.class, args);
+    }
+
+}
