bdbb863f7fa9b41670666bee3d748b95eb35fa9d
Jung Yunho <a01049048063@gmail.com>
2023-03-03 22:22:35 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/springboot/techere/global/config/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.springboot.techere.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {}
