d4a4977c80808c137e4be0f75aadb8a5c5869bd6
Jung Yunho <a01049048063@gmail.com>
2023-03-03 17:52:35 +0900

    chore: Springboot inital setting

---
+++ src/test/java/com/example/techere/TechereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
