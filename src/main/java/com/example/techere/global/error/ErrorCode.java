dd0dd30171cefe2055770c6332a33c4cd1e6e048
halin <halin130@naver.com>
2023-03-15 09:41:03 +0900

    feat: BusinessException 생성자 추가

--- src/main/java/com/example/techere/global/error/ErrorCode.java
+++ src/main/java/com/example/techere/global/error/ErrorCode.java
@@ -1,22 +1,43 @@
 package com.example.techere.global.error;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 /** {주체}_{이유} message 는 동사 명사형으로 마무리 */
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum ErrorCode {
   // Global
-  INTERNAL_SERVER_ERROR(500, "G001", "서버 오류"),
-  INPUT_INVALID_VALUE(400, "G002", "잘못된 입력"),
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E001", "서버 오류"),
+  INPUT_INVALID_VALUE(HttpStatus.BAD_REQUEST, "E002", "잘못된 입력"),
 
   // 예시
   // User 도메인
-  EXAMPLE_USER_ERROR(400, "U001", "테스트용 예시 에러코드"),
+  EXAMPLE_USER_ERROR(HttpStatus.BAD_REQUEST, "U001", "테스트용 예시 에러코드"),
   ;
 
-  private final int status;
+  private final HttpStatus status;
   private final String code;
   private final String message;
+
+  public String getMessage(Throwable e) {
+    return this.getMessage(this.getMessage() + " - " + e.getMessage());
+    // 결과 예시 - "Validation error - Reason why it isn't valid"
+  }
+
+  public String getMessage(String message) {
+    return Optional.ofNullable(message)
+            .filter(Predicate.not(String::isBlank))
+            .orElse(this.getMessage());
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s (%d)", this.name(), this.getCode());
+  }
+
 }
